4807108d338010483931eb15b9dd62972577c797
Bilal Amiche <bilal-amiche_student2021@wilder.school>
2021-11-30 12:07:43 +0100

    oui

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -17,4 +17,21 @@ Class ProgramController extends AbstractController
             'website' => 'Wild Séries',
         ]);
     }
+
+    /**
+    * @Route("/program/{page}", requirements={"page"="\d+"}, name="program_show")
+    */
+    public function show(int $page = 1): Response
+    {
+        return $this->render('program/show.html.twig', ['page' => $page]);
+    }
+
+    public function new(): Response
+    {
+    // traitement d'un formulaire par exemple
+
+    // redirection vers la page 'program_show',
+    // correspondant à l'url /program/4
+    return $this->redirectToRoute('program_show', ['id' => 4]);
+    }
 }
